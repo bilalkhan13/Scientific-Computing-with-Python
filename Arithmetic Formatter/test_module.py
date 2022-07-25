d3c33ef580b586d8672abc4898bc6f849983d345
bilalkhan13 <bilalahmad.khan@hotmail.com>
2022-07-26 00:56:40 +0500

    First Commit

---
+++ Arithmetic Formatter/test_module.py
@@ -0,0 +1,6 @@
+from arithmetic_arranger import arithmetic_arranger
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 / 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["320000 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "3801 - 2", "45 + 43", "123 + 49"]))
