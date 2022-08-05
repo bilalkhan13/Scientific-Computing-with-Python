ec5fadd2cb8500884e2b7a06ec207165c4823ffc
Bilal Ahmad Khan <49161285+bilalkhan13@users.noreply.github.com>
2022-08-05 13:41:53 +0500

    Bug Fixes & recommendations implemented
    Bug Fixes & all valid recommendations implemented

--- Arithmetic Formatter/arithmetic_arranger.py
+++ Arithmetic Formatter/arithmetic_arranger.py
@@ -1,61 +1,107 @@
-def error_checking(operand1, operator, operand2):
-    try:
-        if len(operand1) > 4 or len(operand2) > 4:
-            raise BaseException
-    except:
-        return "Error: Numbers cannot be more than four digits"
-
-    try:
-        if operator != "+" and operator != "-":
-            raise BaseException
-    except:
-        return "Error: Operator must be '+' or '-'."
-
-    try:
-        if operand1 != True or operand2 != True:
-            raise BaseException
-    except:
-        return "Error: Numbers must only contain digits."
-    return " "
+from logging import exception
 
 
-def arithmetic_arranger(arr):
+def arithmetic_arranger(problems, displayMode=False):
+    # Local Variables
+    start = True
+    first_operand_line = ""
+    second_operand_line = ""
+    dashes_line = ""
+    output_line = ""
+
     try:
-        if len(arr) > 5:
-            raise BaseException
+        if len(problems) > 5:
+            raise InvalidProblemsCount
     except:
         return "Error: Too many problems."
-    result = 0
-    res_display = ""
-    line1 = ""
-    line2 = ""
-    line3 = ""
-    space = "------"
 
-    for problem in arr:
-        split_problem = problem.split()
-        operand1 = split_problem[0]
-        operator = split_problem[1]
-        operand2 = split_problem[2]
-        e = error_checking(operand1, operator, operand2)
-        if e != " ":
-            return e
+# Main Loop
+    for problem in problems:
 
-        line1 += operand1.rjust(10, " ")
-        line2 += operator.rjust(6, " ") + " " + operand2.rjust(3, " ")
-        line3 += space.rjust(10, " ")
-        print()
-        if operator == "+":
-            result = int(operand1) + int(operand2)
-            result = str(result)
-            res_display += result.rjust(10, " ")
+        operand1 = splitFunc(problem, 'firstOperand')
+        operator = splitFunc(problem, 'operator')
+        operand2 = splitFunc(problem, 'secondOperand')
+
+        # Exception Handling
+        try:
+            if len(operand1) > 4 or len(operand2) > 4:
+                raise LargeOperandsLength
+            if operator != "+" and operator != "-":
+                raise NotAValidOperator
+            if str(operand1).isnumeric() != True or str(operand2).isnumeric() != True:
+                raise NotADigit
+
+        except LargeOperandsLength:
+            return "Error: Numbers cannot be more than four digits."
+
+        except NotAValidOperator:
+            return "Error: Operator must be '+' or '-'."
+
+        except NotADigit:
+            return "Error: Numbers must only contain digits."
+
+        space = max(len(operand1), len(operand2))
+
+        if start == True:
+            first_operand_line += operand1.rjust(space+2)
+            second_operand_line += operator + " " + operand2.rjust(space)
+            dashes_line += "-" * (space + 2)
+            if displayMode == True:
+                if operator == "+":
+                    output_line += str(int(operand1) +
+                                       int(operand2)).rjust(space + 2)
+                else:
+                    output_line += str(int(operand1) -
+                                       int(operand2)).rjust(space + 2)
+            start = False
         else:
-            result = int(operand1) - int(operand2)
-            result = str(result)
-            res_display += result.rjust(10, " ")
+            first_operand_line += str(operand1).rjust(space+6)
+            second_operand_line += str(operator).rjust(5) + \
+                " " + str(operand2).rjust(space)
+            dashes_line += "    " + "-" * (space+2)
+            if displayMode == True:
+                if operator == "+":
+                    output_line += "    " + \
+                        str(int(operand1) + int(operand2)).rjust(space + 2)
+                else:
+                    output_line += "    " + \
+                        str(int(operand1) - int(operand2)).rjust(space + 2)
 
-    print(line1)
-    print(line2)
-    print(line3)
-    print(res_display)
-    return ""
+    # Display Output
+    if displayMode == True:
+        return first_operand_line + "\n" + second_operand_line + "\n" + dashes_line + "\n" + output_line
+    return first_operand_line + "\n" + second_operand_line + "\n" + dashes_line
+
+# Split Function
+
+
+def splitFunc(problem, arg):
+    split_problem = problem.split()
+    if arg == "firstOperand":
+        return split_problem[0]
+    elif arg == "operator":
+        return split_problem[1]
+    else:
+        return split_problem[2]
+
+# Exception Classes
+
+
+class Error (Exception):
+    pass
+
+
+class LargeOperandsLength(Error):
+    pass
+
+
+class NotAValidOperator(Error):
+    pass
+
+
+class InvalidProblemsCount(Error):
+    pass
+
+
+class NotADigit(Error):
+    pass
