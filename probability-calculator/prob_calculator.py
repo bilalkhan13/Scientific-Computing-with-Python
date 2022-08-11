5561d69611286c3504dd5cde0daef15a1c6e0354
bilalkhan13 <bilalahmad.khan@hotmail.com>
2022-08-11 23:13:36 +0500

    Update

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -1,21 +1,41 @@
 from random import random, randrange
-
+import copy
 
 class Hat:
   def __init__(self, **all_items):
-    self.content = []
+    self.contents = []
     for key, value in all_items.items():
       for n in range(value):
-        self.content.append(key)
-    print(self.content)
+        self.contents.append(key)
+    print(self.contents)
 
-  def draw(self, number):
-    draw_list = []
-    if number >= len(self.content):
-      return self.content
-    for itr in range(number):
-      name = self.content.pop(random.randrange(len(self.content)))
-      draw_list.append(name)
+    self.__intial_contents = copy.copy(self.contents)
+
+  def reset(self):
+    self.contents = copy.copy(self.__intial_contents)
+
+  def draw(self, count):
+    # Draw the specified count of items from the hat, return all items if the count is > number of items
+    try:
+      drawn = random.sample(self.contents, count)
+    except ValueError:
+      drawn = copy.copy(self.contents)
+
+    # Remove the items drawn from the hat
+    for item in drawn:
+      self.contents.remove(item)
+
+    # Refill the hat if empty
+    if len(self.contents) == 0:
+      self.contents = copy.copy(self.__initial_contents)
+
+    return drawn
+
+    # if count >= len(self.contents):
+    #   return self.contents
+    # for itr in range(count):
+    #   name = self.contents.pop(random.randrange(len(self.contents)))
+    #   draw_list.append(name)
     return draw_list
 
 
