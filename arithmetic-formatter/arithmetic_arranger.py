a2bc751c5f6910552e17168f8576b5e603c0d200
bilalkhan13 <bilalahmad.khan@hotmail.com>
2022-08-16 23:28:45 +0500

    Code improvements implemented

--- arithmetic-formatter/arithmetic_arranger.py
+++ arithmetic-formatter/arithmetic_arranger.py
@@ -1,14 +1,13 @@
 def arithmetic_arranger(problems, displayMode=False):
-    first_operand_line = second_operand_line = dashes_line = ""
-    result_line = ""
-
+    first_operand_line = second_operand_line = dashes_line = result_line = ""
+    result_new_line = "\n"
+    if len(problems) > 5:
+        return "Error: Too many problems."
     for index in range(len(problems)):
         problem = problems[index]
         [operand1, operator, operand2] = problem.split()
 
         # Error Handling
-        if len(problems) > 5:
-            return "Error: Too many problems."
         if len(operand1) > 4 or len(operand2) > 4:
             return "Error: Numbers cannot be more than four digits."
         if operator != "+" and operator != "-":
@@ -17,27 +16,18 @@ def arithmetic_arranger(problems, displayMode=False):
             return "Error: Numbers must only contain digits."
 
         operand_max_length = max(len(operand1), len(operand2))
+        before_line_spaces = operand_max_length + 2
+
         if index == 0:
-            first_line_before_spaces = operand_max_length+2
-            second_line_before_spaces = operand_max_length + 1
-            before_operator_Spaces = 0
-            dashes_line_before_spaces = operand_max_length+2
-            spaces_before_result = ""
-            result_new_line = "\n"
+            spaces_before_result_dashes = ""
         else:
-            first_line_before_spaces = operand_max_length+6
-            second_line_before_spaces = operand_max_length+1
-            before_operator_Spaces = 5
-            dashes_line_before_spaces = operand_max_length+2
-            spaces_before_result = " " * 4
+            spaces_before_result_dashes = " " * 4
             result_new_line = ""
 
-        first_operand_line += operand1.rjust(first_line_before_spaces)
-        second_operand_line += operator.rjust(before_operator_Spaces) + operand2.rjust(second_line_before_spaces)
-        dashes_line += spaces_before_result + "-" * (dashes_line_before_spaces)
+        first_operand_line += spaces_before_result_dashes + operand1.rjust(before_line_spaces)
+        second_operand_line += spaces_before_result_dashes + operator + operand2.rjust(before_line_spaces - 1)
+        dashes_line += spaces_before_result_dashes + "-" * (before_line_spaces)
         if displayMode == True:
-            result_line += result_new_line
-            result_line += spaces_before_result + str(int(operand1) + int(operand2)).rjust(
-                dashes_line_before_spaces) if operator == "+" else spaces_before_result+str(int(operand1) - int(operand2)).rjust(dashes_line_before_spaces)
+            result_line +=  result_new_line + spaces_before_result_dashes + str((int(operand1) + int(operand2) if operator == "+" else int(operand1) - int(operand2))).rjust(before_line_spaces)
 
     return first_operand_line + "\n" + second_operand_line + "\n" + dashes_line + result_line
