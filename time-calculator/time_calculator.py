002b5fc64ccf97998cc760a68bd94dc230ba4511
bilalkhan13 <bilalahmad.khan@hotmail.com>
2022-09-13 23:22:42 +0500

    Code Optimization & implement best practices.

--- time-calculator/time_calculator.py
+++ time-calculator/time_calculator.py
@@ -1,55 +1,43 @@
-def add_time(start_time, stop_time, day_of_week=None):
+def add_time(start_full_time, stop_full_time, day_of_week=None):
     PM = "PM"
-    [intial_time, start_day_unit] = get_time(start_time)
-    [start_hours, start_minutes] = get_hours_minutes(intial_time)
-
-    [stop_hours, stop_minutes] = get_hours_minutes(stop_time)
-
-    stop_minutes = into_minutes(stop_minutes, stop_hours)
+    [initial_time, start_day_unit] = get_time(start_full_time)
+    stop_minutes = get_minutes(stop_full_time)
+    start_minutes = get_minutes(initial_time)
 
     if start_day_unit == PM:
-        start_hours = start_hours + 12
+        start_minutes += 12 * 60
 
-    start_minutes = into_minutes(start_minutes, start_hours)
-    total_minutes = start_minutes + stop_minutes
+    result_minutes = start_minutes + stop_minutes
+    final_time_minutes = result_minutes % 60
+    final_time_minutes = (f"{final_time_minutes:02d}") if final_time_minutes < 10 else final_time_minutes
+    result_hours = int(result_minutes / 60)
 
-    final_minutes = total_minutes % 60
-    total_hours = int(total_minutes / 60)
 
-    if final_minutes < 10:
-        final_minutes = "0" + str(final_minutes)
-    else:
-        final_minutes = str(final_minutes)
-
-    hours = total_hours % 24
-    days = int(total_hours / 24)
-
-    final_hours = hours % 12
+    hours = result_hours % 24
+    days = int(result_hours / 24)
+    final_time_hours = hours % 12
 
     if int(hours / 12) == 0:
-        am_or_pm = "AM"
-
-        if final_hours == 0:
-            final_hours = 12
+        final_time_unit = "AM"
     else:
-        am_or_pm = PM
+        final_time_unit = PM
 
-        if final_hours == 0:
-            final_hours = 12
+    if final_time_hours == 0:
+        final_time_hours = 12
 
-    time_stamp = str(final_hours) + ":" + final_minutes + " " + am_or_pm
+    time_stamp = str(final_time_hours) + ":" + str(final_time_minutes) + " " + final_time_unit
 
     if not day_of_week == None:
         days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-        pos = 0
+        position = 0
 
         while True:
-            if day_of_week.lower() == days_of_week[pos].lower():
+            if day_of_week.lower() == days_of_week[position].lower():
                 break
 
-            pos = pos + 1
+            position = position + 1
 
-        newDay_of_week = days_of_week[((pos + (days % 7)) % 7)]
+        newDay_of_week = days_of_week[((position + (days % 7)) % 7)]
         time_stamp += ", " + newDay_of_week
 
     if not days == 0:
@@ -57,16 +45,9 @@ def add_time(start_time, stop_time, day_of_week=None):
 
     return time_stamp
 
+def get_time(time):
+    return time.split()
 
-def get_time(get_time):
-    [time, start_day_unit] = get_time.split()
-    return time, start_day_unit
-
-
-def get_hours_minutes(get_time):
-    [hours, minutes] = get_time.split(":")
-    return int(hours), int(minutes)
-
-
-def into_minutes(minutes, hours):
-    return int(int(minutes) + (60 * int(hours)))
+def get_minutes(time):
+    [hours, minutes] = time.split(":")
+    return int(minutes) + (60 * int(hours))
