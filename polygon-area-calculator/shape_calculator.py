8d24157ab0a52f1d25caa300e18d8bd16d918672
bilalkhan13 <bilalahmad.khan@hotmail.com>
2022-09-14 16:23:50 +0500

    Code optimization implemented.

--- polygon-area-calculator/shape_calculator.py
+++ polygon-area-calculator/shape_calculator.py
@@ -10,26 +10,24 @@ class Rectangle:
         self.height = val
 
     def get_area(self):
-        return (self.width * self.height)
+        return self.width * self.height
 
     def get_perimeter(self):
-        return ((2 * self.width + 2 * self.height))
+        return 2 * self.width + 2 * self.height
 
     def get_diagonal(self):
-        return ((self.width ** 2 + self.height ** 2) ** .5)
+        return (self.width ** 2 + self.height ** 2) ** .5
 
     def get_picture(self):
-
         if self.width > 50 or self.height > 50:
             return "Too big for picture."
-        else:
-            return ((("*" * self.width) + "\n") * self.height)
+        return (("*" * self.width) + "\n") * self.height
 
     def get_amount_inside(self, shape):
         return (self.width // shape.width) * (self.height // shape.height)
 
     def __str__(self):
-        return (("Rectangle(width={0}, height={1})").format(self.width, self.height))
+        return "Rectangle(width={0}, height={1})".format(self.width, self.height)
 
 class Square(Rectangle):
     def __init__(self, side):
@@ -39,7 +37,7 @@ class Square(Rectangle):
     def set_side(self, val):
         self.side = self.width = self.height = val
 
-    def set_width(self,value):
+    def set_width(self, value):
         self.set_side(value)
 
     def set_height(self,value):
